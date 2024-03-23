cdf1027010581564c79b114bd52e7bc0babc0549
Tharuksha <tharukshawickramarachchi@gmail.com>
2024-03-23 10:01:50 +0530

    baseURL Update

--- src/services/AxiosOrder.jsx
+++ src/services/AxiosOrder.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL:
-    "https://f6e1-2402-1f00-8001-203e-26d7-bf13-d63b-1.ngrok-free.app/api",
+    "https://4dd8-2a09-bac1-4360-00-23-409.ngrok-free.app/api",
 });
 
 export default instance;
