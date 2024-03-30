fd8d21f5e054c0be31c35e670fda6a46ebcb964b
Tharuksha <tharukshawickramarachchi@gmail.com>
2024-03-30 23:18:54 +0530

    .

--- src/services/AxiosOrder.jsx
+++ src/services/AxiosOrder.jsx
@@ -6,7 +6,7 @@ export const instance = axios.create({
 });
 
 export const instance2 = axios.create({
-  baseURL: "http://localhost:8181/api",
+  baseURL: "https://d43f-2402-1f00-8001-203e-26d7-bf13-d63b-1.ngrok-free.app/api",
 });
 
 export const instance3 = axios.create({
