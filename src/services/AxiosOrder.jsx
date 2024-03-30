41515260ce25b2814a8ac782cadefed3187b07f5
Tharuksha <tharukshawickramarachchi@gmail.com>
2024-03-30 23:11:15 +0530

    Update API base URLs

--- src/services/AxiosOrder.jsx
+++ src/services/AxiosOrder.jsx
@@ -2,11 +2,11 @@ import axios from "axios";
 
 export const instance = axios.create({
   baseURL:
-    "http://localhost:8080/api",
+    "https://ccfb-2402-1f00-8001-203e-26d7-bf13-d63b-1.ngrok-free.app/api",
 });
 
 export const instance2 = axios.create({
-  baseURL: "http://localhost:8181/api",
+  baseURL: "https://3864-2402-1f00-8001-203e-26d7-bf13-d63b-1.ngrok-free.app/api",
 });
 
 export const instance3 = axios.create({
