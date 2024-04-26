5b1976e12522ce8801e8cb675c14ff3750ff6417
Tharuksha <tharukshawickramarachchi@gmail.com>
2024-04-26 18:46:14 +0530

    Refactor handleDeleteOrder function and update order ID references in Orders.jsx

--- src/pages/DonateBooks/DonateBooks.jsx
+++ src/pages/DonateBooks/DonateBooks.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
-import { Trash2, NotebookPen, X } from "lucide-react";
 import { instance } from "../../services/AxiosOrder";
+import { Trash2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import toast, { Toaster } from "react-hot-toast";
 import {
   Table,
@@ -11,392 +11,88 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogDescription,
-  DialogFooter,
-} from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
+import { useState, useEffect } from "react";
+
+export default function Donations() {
+  const [donations, setDonations] = useState([]);
 
 
-export default function DonateBooks() {
-
-  const [showAddDialog, setShowAddDialog] = useState(false);
-  const [showEditDialog, setShowEditDialog] = useState(false);
-  const [selectedBook, setSelectedBook] = useState(null);
-  const [allBooks, setAllBooks] = useState([]);
-
-  const [newBook, setNewBook] = useState({
-    bookName: "",
-    bookAuthor: "",
-    publisher: "",
-    isbn: "",
-    addedOn: null,
-    quantity: 0,
-  });
-  // show all the books in a array list
   useEffect(() => {
     instance
       .get("/donations/all")
       .then((response) => {
         if (Array.isArray(response.data)) {
-          setAllBooks(response.data);
+          setDonations(response.data);
+          console.log("Donations:", response.data);
         } else {
-          console.log(
-            "Data is not available in array: ",
-            typeof response.data
-          );
+          console.log(response.data);
         }
       })
       .catch((error) => {
-        console.error("Error loading Books: ", error);
+        console.error("Error loading Donations:", error);
       });
   }, []);
 
-
-  //save new book 
-  const handleSaveNewBook = async () => {
-    try {
-      const response = await instance.post("/books/add", {
-        bookName: newBook.bookName,
-        bookAuthor: newBook.bookAuthor,
-        publisher: newBook.publisher,
-        isbn: newBook.isbn,
-        addedOn: new Date().toISOString().substring(0, 10),
-        quantity: newBook.quantity
-      });
-
-      //success message
-      console.log("New book added:", response.data);
-      toast.success("New book added successfully");
-      setNewBook({
-        bookName: "",
-        bookAuthor: "",
-        publisher: "",
-        isbn: "",
-        addedOn: null,
-        quantity: 0,
-      });
-      updatedBookList();
-    }
-    catch (error) {
-      if (error.response) {
-        // Request was made and server responded with a status code
-        // that falls out of the range of 2xx
-        console.error("Server responded with error status:", error.response.status);
-        console.error("Error data:", error.response.data);
-        toast.error("Server responded with an error. Please try again later.");
-      } else if (error.request) {
-        // The request was made but no response was received
-        console.error("No response received from the server.");
-        toast.error("No response received from the server. Please try again later.");
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.error("Error setting up the request:", error.message);
-        toast.error("An unexpected error occurred. Please try again later.");
-      }
-    }
-  };
-
-
-
-
-
-  // delete a book
-  const handleDeleteBook = async (bookId) => {
+  const handleDeleteDonations = async (donationId) => {
 
     try {
-      await instance.delete(`/books/delete/${bookId}`);
-      console.log("Book deleted:", bookId);
-      toast.success("Book deleted successfully");
+      await instance.delete(`/donations/${donationId}`);
+      console.log("Donation deleted:", donationId);
+      toast.success("Donation deleted successfully");
       // Optionally, update the list of books after deleting
-      updatedBookList();
+      updatedDonationList();
     } catch (error) {
-      console.error("Error deleting book: ", error);
-      toast.error("Error deleting book");
+      console.error("Error deleting Donation: ", error);
+      toast.error("Error deleting Donation");
     }
   };
 
-
-  //Showing add book page
-  const handleAddBook = () => {
-    setShowAddDialog(true);
-  };
-
-  //Show edit book page
-  // const handleEditBook = (bookId) => {
-  //   setSelectedBook(bookId);
-  //   setShowEditDialog(true);
-  // };
-
-  const handleEditBook = (book) => {
-    setSelectedBook(book); // Set the entire book object here
-    setShowEditDialog(true);
-  };
-
-
-  // update existing book
-  // update existing book
-const handleUpdateBook = async () => {
-  try {
-    const response = await instance.put(`/books/update/${selectedBook.bookId}`, selectedBook);
-    console.log("Book updated:", response.data);
-    toast.success("Book updated successfully");
-    // Update the list of books after updating
-    updatedBookList();
-  } catch (error) {
-    console.error("Error updating book: ", error);
-    toast.error("Error updating book");
-  }
-};
-
-
-  // function to fetch updated list of books
-  const updatedBookList = async () => {
+   // function to fetch updated list of books
+   const updatedDonationList = async () => {
     try {
-      const response = await instance.get("/books/getAll");
-      setAllBooks(response.data);
+      const response = await instance.get("/donations/all");
+      setDonations(response.data);
     } catch (error) {
-      console.error("Error updating book list: ", error);
+      console.error("Error updating Donation list: ", error);
     }
   };
 
   return (
     <>
-      <Toaster />
-      <div className="flex justify-end mb-5">
-        <Button
-          className="m-0 w-[150px] h-[50px] mr-[10px] text-lg"
-          type="button"
-          onClick={handleAddBook}
-        >
-          Add New Book
-        </Button>
-      </div>
-      <Table>
-        <TableCaption>Manage Book Store</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Book ID</TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead>Author</TableHead>
-            <TableHead>Publisher</TableHead>
-            <TableHead>ISBN</TableHead>
-            <TableHead>Added On</TableHead>
-            <TableHead>Quantity</TableHead>
-            <TableHead>Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {allBooks.map((book) => (
-            <TableRow key={book.bookId}>
-              <TableCell>{book.bookId}</TableCell>
-              <TableCell>{book.bookName}</TableCell>
-              <TableCell>{book.bookAuthor}</TableCell>
-              <TableCell>{book.publisher}</TableCell>
-              <TableCell>{book.isbn}</TableCell>
-              <TableCell>{book.addedOn}</TableCell>
-              <TableCell>{book.quantity}</TableCell>
-              <TableCell>
+    <Toaster />
+    <Table>
+      <TableCaption>All Donations</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>ID</TableHead>
+          <TableHead>Author</TableHead>
+          <TableHead>Book Name</TableHead>
+          <TableHead>Donation Type</TableHead>
+          <TableHead>ISBN</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {donations.map((donation) => (
+          <TableRow key={donation.Id}>
+            <TableCell>{donation.donationId}</TableCell>
+            <TableCell>{donation.author}</TableCell>
+            <TableCell>{donation.bookName}</TableCell>
+            <TableCell>{donation.donationType}</TableCell>
+            <TableCell>{donation.isbn}</TableCell>
+            <TableCell>
                 <>
                   <Button
-                    onClick={() => handleDeleteBook(book.bookId)}
+                    onClick={() => handleDeleteDonations(donation.donationId)}
                     variant="destructive"
                     size="icon">
                     <Trash2 size={20} />
                   </Button>
-                  {"  "}
-                  <Button
-                    onClick={() => handleEditBook(book)}
-                    variant="secondary"
-                    size="icon">
-                    <NotebookPen size={20} />
-                  </Button>
                 </>
               </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-
-      {showAddDialog && (
-        <Dialog open>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Add New Book</DialogTitle>
-              <Button
-                onClick={() => setShowAddDialog(false)}
-                aria-label="Close"
-                className="absolute top-0 right-0 mt-2 mr-2 w-8 h-8"
-                style={{ lineHeight: "1", padding: "0" }}
-              >
-                <X size={24} />
-              </Button>
-            </DialogHeader>
-
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="book_name">Book Name</Label>
-                <Input
-                  id="book_name"
-                  value={newBook.bookName}
-                  onChange={(e) =>
-                    setNewBook({ ...newBook, bookName: e.target.value })
-                  }
-                  className="col-span-3"
-                  required
-                />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="author">Author</Label>
-                <Input
-                  id="author"
-                  value={newBook.bookAuthor}
-                  onChange={(e) =>
-                    setNewBook({ ...newBook, bookAuthor: e.target.value })
-                  }
-                  className="col-span-3"
-                  required
-                />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="publisher">Publisher</Label>
-                <Input
-                  id="publisher"
-                  value={newBook.publisher}
-                  onChange={(e) =>
-                    setNewBook({ ...newBook, publisher: e.target.value })
-                  }
-                  className="col-span-3"
-                  required
-                />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="isbn">ISBN</Label>
-                <Input
-                  id="isbn"
-                  value={newBook.isbn}
-                  onChange={(e) =>
-                    setNewBook({ ...newBook, isbn: e.target.value })
-                  }
-                  className="col-span-3"
-                  required
-                />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="quantity">Quantity</Label>
-                <Input
-                  id="quantity"
-                  value={newBook.quantity}
-                  onChange={(e) =>
-                    setNewBook({ ...newBook, quantity: e.target.value })
-                  }
-                  className="col-span-3"
-                  required
-                />
-              </div>
-              {/* <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="addedOn">Added On</Label>
-
-                <DatePicker id="addedOn"
-                  value={newBook.addedOn}
-                  onSelect={(e) =>
-                    setNewBook({ ...newBook, addedOn: e.target.value })}
-                />
-              </div> */}
-            </div>
-            <DialogFooter>
-              <Button
-                type="button"
-                onClick={handleSaveNewBook}
-              >
-                Save Book
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      )}
-
-      {showEditDialog && (
-        <Dialog open>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Edit Book Details</DialogTitle>
-              <DialogDescription>
-                Make the necessary changes related to books here.
-              </DialogDescription>
-              <Button
-                onClick={() => {
-                  setShowEditDialog(false);
-                }}
-                aria-label="Close"
-                className="absolute top-0 right-0 mt-2 mr-2 w-8 h-8"
-                style={{ lineHeight: "1", padding: "0" }}
-              >
-                <X size={24} />
-              </Button>
-            </DialogHeader>
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="book_name">Book Name</Label>
-                <Input
-                  id="book_name"
-                  value={selectedBook?.bookName || ''}
-                  onChange={(e) => setSelectedBook({ ...selectedBook, bookName: e.target.value })}
-                  className="col-span-3"
-                />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="author">Author</Label>
-                <Input
-                  id="author"
-                  value={selectedBook?.bookAuthor || ''}
-                  onChange={(e) => setSelectedBook({ ...selectedBook, bookAuthor: e.target.value })}
-                  className="col-span-3"
-                />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="publisher">Publisher</Label>
-                <Input
-                  id="publisher"
-                  value={selectedBook?.publisher || ''}
-                  onChange={(e) => setSelectedBook({ ...selectedBook, publisher: e.target.value })}
-                  className="col-span-3"
-                />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="isbn">ISBN</Label>
-                <Input
-                  id="isbn"
-                  value={selectedBook?.isbn || ''}
-                  onChange={(e) => setSelectedBook({ ...selectedBook, isbn: e.target.value })}
-                  className="col-span-3"
-                />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="quantity">Quantity</Label>
-                <Input
-                  id="quantity"
-                  value={selectedBook?.quantity || ''}
-                  onChange={(e) => setSelectedBook({ ...selectedBook, quantity: e.target.value })}
-                  className="col-span-3"
-                />
-              </div>
-            </div>
-            <DialogFooter>
-              <Button type="button" onClick={handleUpdateBook}>
-                Save changes
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      )}
-
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
     </>
   );
 }
